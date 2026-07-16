bd3c4b9da3eeb8424fef8043a3efe09a219c67d7
SwarupNanche <140883044+SwarupNanche@users.noreply.github.com>
2026-07-16 12:38:16 +0530

    Create Churro_Activity_Date.sql

---
+++ Churro_Activity_Date.sql
@@ -0,0 +1,7 @@
+-- ID 9688
+-- Find the inspection date and risk category (pe_description) of facilities named 'STREET CHURROS' that received a score below 95.
+
+select  
+activity_date,pe_description
+from los_angeles_restaurant_health_inspections
+where facility_name='STREET CHURROS' and score<95
